e374bd01d62f530d096e5425023e40af6c5a0420
diegomartinez2 <diegomartinez2@yahoo.es>
2026-02-17 08:08:20 +0100

    Fix error in header

--- ALAMODE/VASP_LAMMPS_converter.py
+++ ALAMODE/VASP_LAMMPS_converter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  poscar_direct_to_cartesian.py
+#  VASP_LAMMPS_converter.py
 #
 #  Copyright 2026 Diego Martinez Gutierrez <diego.martinez@ehu.eus>
 #
